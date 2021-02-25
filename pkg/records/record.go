139c303bf84c9b3cfe4670cfbce70cfc4519dc88
Ivan Rylach <ivan.rylach@gmail.com>
2021-02-25 15:56:53 -0800

    revet unmarshalling

--- pkg/records/record.go
+++ pkg/records/record.go
@@ -1,9 +1,7 @@
 package records
 
-import "encoding/json"
-
 type RecordDTO struct {
 	Id          string `json:"id" bson:"id"`
-	Name        json.RawMessage `json:"name,string" bson:"name" binding:"required"`
+	Name        string `json:"name" bson:"name" binding:"required"`
 	Description string `json:"description" bson:"description,omitempty" binding:"required"`
 }
